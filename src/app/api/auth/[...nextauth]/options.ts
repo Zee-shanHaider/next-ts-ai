1349ba247f02e3ff9c721fdd0b89a914d533189e
zeeshan <zeeshan9402529@gmail.com>
2024-07-12 11:01:56 +0500

    signin

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,57 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnection";
+import UserModel from "@/app/model/User";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: "Credentials",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any) {
+        try {
+          await dbConnect();
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { username: credentials.identifier },
+            ],
+          });
+          if (!user) throw new Error("User not found");
+          if (!user.isVerified)
+            throw new Error("Please verify your account before login");
+          const comparePassword = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (comparePassword) return user;
+          else throw new Error("Incorrect Password");
+        } catch (error: any) {
+          throw new Error(error.message);
+        }
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+        // You can also use the `req` object to obtain additional parameters
+        // (i.e., the request IP address)
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET_KEY,
+};
